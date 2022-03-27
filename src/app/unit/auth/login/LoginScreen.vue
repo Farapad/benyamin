417f099523eb6fc479420553cfb3d83e9d7437a9
benyamin parsa <benyaminparsa727@gmail.com>
2022-03-27 12:55:40 +0430

    fix login

--- src/app/unit/auth/login/LoginScreen.vue
+++ src/app/unit/auth/login/LoginScreen.vue
@@ -1,10 +1,35 @@
 <template>
-  <div>
-    <h2 class="text-center">Login is work ...</h2>
+  <div class="main-controller">
+    <div><topscreen :label="'ورود به سامانه'"/></div>
+    <div>
+      <InputText type="text" v-model="username" />
+      <Password v-model="value1" :feedback="false" />
+    </div>
+    <div></div>
   </div>
 </template>
 <script lang="ts">
-export default {};
+import topscreen from '@/app/unit/auth/topscreen.vue'
+import InputText from 'primevue/inputtext';
+import Password from 'primevue/password';
+export default {
+  components:{topscreen,InputText,Password},
+  setup(){
+    const username = null;
+    const password = null;
+    return {
+      username,
+      password
+    }
+  }
+
+};
 </script>
 <style lang="scss" scoped>
+.main-controller{
+  display:flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+}
 </style>
