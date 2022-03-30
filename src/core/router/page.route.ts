77cb5ac830d490fab3385cbeae0bb2f39e83cf28
benyamin parsa <benyaminparsa727@gmail.com>
2022-03-30 19:44:28 +0430

    fix main page

--- src/core/router/page.route.ts
+++ src/core/router/page.route.ts
@@ -2,9 +2,12 @@ import { RouteRecordRaw } from "vue-router";
 import { canActivatedGuard } from "@/core/router/router";
 
 const Pages = () => import("@/app/unit/pages/Pages.vue");
-// const About = () => import("@/app/unit/pages/About-US/aboutus.vue");
+const courses = () => import("@/app/unit/pages/courses/coursesScreen.vue");
+
+const About = () => import("@/app/unit/pages/About-US/aboutus.vue");
 
 export const pageRoutes: Array<RouteRecordRaw> = [
   { path: "/pages", component: Pages, beforeEnter: [canActivatedGuard] },
-  //   { path: "/about", component: About },
+  { path: "/courses", component: courses },
+  { path: "/about", component: About },
 ];
