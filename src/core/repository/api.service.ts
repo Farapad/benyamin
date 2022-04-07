3234867dbb29b59c895bbe9ee7251a7216007f2b
benyamin parsa <benyaminparsa727@gmail.com>
2022-04-07 15:12:24 +0430

    fix api

--- src/core/repository/api.service.ts
+++ src/core/repository/api.service.ts
@@ -1,22 +1,46 @@
-import { Constant } from '../constant/constant';
-import { Repository, ReturnResponse } from './repository';
+import { Constant } from "../constant/constant";
+import { Repository, ReturnResponse } from "./repository";
 
 export class ApiService extends Repository {
-    private static instance: ApiService
-    public baseUrl = Constant.ApiPrefix
+  private static instance: ApiService;
+  public baseUrl = Constant.ApiPrefix;
 
-    public static getInstance(): ApiService {
-        if (!ApiService.instance) ApiService.instance = new ApiService()
-        return ApiService.instance
+  public static getInstance(): ApiService {
+    if (!ApiService.instance) ApiService.instance = new ApiService();
+    return ApiService.instance;
+  }
+
+  private encodeQueryData(data) {
+    const ret = [];
+    for (let d in data)
+      ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
+    return ret.join("&");
+  }
+
+  private setParams(data: ReturnResponse) {
+    let params = "";
+    if (data.params) {
+      params += "?";
+      params = this.encodeQueryData(data.params ?? {});
     }
+    return params;
+  }
 
-    private compeleteUrl(data: ReturnResponse, apiUrl: string): string {
-        return `${data.mainUrl ?? Constant.ApiPrefix}/${apiUrl}`
-    }
+  private compeleteUrl(
+    data: ReturnResponse,
+    apiUrl: string,
+    useSlash = true
+  ): string {
+    return `${data.mainUrl ?? Constant.ApiPrefix}${
+      useSlash ? "/" : ""
+    }${apiUrl}${this.setParams(data)}`;
+  }
 
-    public business(data: ReturnResponse) {
-        data.url = this.compeleteUrl(data, 'user/business');
-        this.post(data)
-    }
+  public business(data: ReturnResponse) {
+    console.log("data.url");
+    data.url = this.compeleteUrl(data, "", false);
+    console.log(data.url);
 
+    this.post(data);
+  }
 }
