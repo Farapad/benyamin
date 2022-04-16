929fe6ea96ce619ffe7300128598cfd11731d3e0
benyamin parsa <benyaminparsa727@gmail.com>
2022-04-16 15:37:20 +0430

    fix  Applicant

--- src/core/router/page.route.ts
+++ src/core/router/page.route.ts
@@ -3,11 +3,13 @@ import { canActivatedGuard } from "@/core/router/router";
 
 const Pages = () => import("@/app/unit/pages/Pages.vue");
 const courses = () => import("@/app/unit/pages/courses/coursesScreen.vue");
-
 const About = () => import("@/app/unit/pages/About-US/aboutus.vue");
+const Applicant = () =>
+  import("@/app/unit/pages/ApplicantScreen/Applicant.vue");
 
 export const pageRoutes: Array<RouteRecordRaw> = [
   { path: "/pages", component: Pages, beforeEnter: [canActivatedGuard] },
   { path: "/courses", component: courses },
   { path: "/about", component: About },
+  { path: "/Applicant", component: Applicant },
 ];
