5ec8b43c4f25deb1cb955fadc9c5ea74f197e29b
benyamin parsa <benyaminparsa727@gmail.com>
2022-04-21 16:06:51 +0430

    fix

--- src/app/unit/pages/myLessonsScreen/MyLessons.vue
+++ src/app/unit/pages/myLessonsScreen/MyLessons.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="main-controller">
-      <div class="controller">
-
+      <div class="top-bar">
+          <div class="first-child">
+              <h1>درس های من</h1>
+          </div>
+          <div class="last-child">
+              <ul>
+                  <li>عنوان درس</li>
+                  <li>شناسه درس</li>
+                  <li>عنوان دوره </li>
+                  <li>عنوان گروه آموزشی</li>
+              </ul>
+          </div>
       </div>
   </div>
 </template>
@@ -13,5 +23,37 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-
+.main-controller{
+    .top-bar{
+        .first-child {
+           h1{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            color: rgba(31, 41, 55, 1);
+            font-size: 14px  !important;
+            font-weight:bolder;
+           } 
+        }
+        .last-child{
+            ul {
+                display: flex;
+                list-style: none;
+                justify-content: center;
+                align-items: center;
+                padding: 0px !important;
+                background: rgba(229, 231, 235, 1);
+                height: 49px;
+                li{
+                    font-size: 12px;
+                    padding: 8px 16px;
+                    border-left: 1px solid  rgba(209, 213, 219, 1);
+                    :last-child{
+                        border: none !important;
+                    }
+                }
+            }
+        }
+    }
+}
 </style>
