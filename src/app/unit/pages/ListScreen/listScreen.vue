1b128d35ea62f582fc6fc2f54c999c649cc250b8
benyamin parsa <benyaminparsa727@gmail.com>
2022-04-25 17:37:08 +0430

    fix

--- src/app/unit/pages/ListScreen/listScreen.vue
+++ src/app/unit/pages/ListScreen/listScreen.vue
@@ -3,13 +3,18 @@
         <h5 class="header-text">لیست گواهینامه های ارسالی توسط مدرسان</h5>
         <Accordion :multiple="true" >
             <AccordionTab header="گواهینامه محمد محمدی">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div class="d-flex">
+                    <div class="w-50 first-button">
+                        <Button :label="'پرینت گواهینامه'"/>
+                    </div>
+                    <div class="w-50 last-button">
+                        <Button :label="'بارگذاری گواهینامه'"/>
+                    </div>
+                </div>
             </AccordionTab>
             <AccordionTab header="گواهینامه محمد محمدی">
-                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.</p>
             </AccordionTab>
             <AccordionTab header="گواهینامه محمد محمدی">
-                <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.</p>
             </AccordionTab>
         </Accordion>
     </div>    
@@ -32,6 +37,26 @@ export default {
         margin-bottom: 8px;
     }
 }
+
+.first-button{
+    display: flex;
+    justify-content: center;
+    button{
+        background: rgba(253, 175, 59, 1);
+        border: none;
+        font-size: 14px;
+    }
+}
+.last-button{
+    display: flex;
+    justify-content: center;
+    button{
+        background: rgba(87, 204, 153, 1);
+        border: none;
+        font-size: 14px;
+    }
+}
+
 
 ::v-deep(.p-accordion .p-accordion-header .p-accordion-header-link .p-accordion-toggle-icon) {
     position: relative;
