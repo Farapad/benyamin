4901b39845ec8b9d899b2b5371f64c7cc7ecbf0c
benyamin parsa <benyaminparsa727@gmail.com>
2022-04-30 19:05:11 +0430

    fix

--- src/app/unit/pages/courses/coursesScreen.vue
+++ src/app/unit/pages/courses/coursesScreen.vue
@@ -11,16 +11,6 @@
    </div>
  <div class="card">
   <DataView :value="products"  :paginator="true" :layout="layout"  :rows="12" :sortOrder="sortOrder" :sortField="sortField">
-    <!-- <template #header>
-                <div class="grid grid-nogutter">
-                    <div class="col-6" style="text-align: left">
-                        <Dropdown v-model="sortKey" :options="sortOptions" optionLabel="label" placeholder="Sort By Price" @change="onSortChange($event)"/>
-                    </div>
-                    <div class="col-6" style="text-align: right">
-                        <DataViewLayoutOptions v-model="layout" />
-                    </div>
-                </div>
-	  </template> -->
       <template #grid="slotProps">
       <div class="m-3 card-controller">
           <img class="card-controller-img" :src="slotProps.data.image" :alt="slotProps.data.name"/>
@@ -104,20 +94,20 @@ export default {
         {id:1001,code: "cdcdc",name: 'cdcdcd cdc',image: picture.value ,description: 'Product Description'},
         {id:1001,code: "cdcdc",name: 'cdcdcd cdc',image: picturee.value ,description: 'Product Description'},
       ];
-           const sortKey = ref();
-            const sortOrder = ref();
-            const sortField = ref();
-               const sortOptions = ref([
-                    {label: 'Price High to Low', value: '!price'},
-                    {label: 'Price Low to High', value: 'price'},
-                ]);
+      const sortKey = ref();
+
+      const sortOrder = ref();
+
+      const sortField = ref();
+
+      const sortOptions = ref([
+            {label: 'Price High to Low', value: '!price'},
+            {label: 'Price Low to High', value: 'price'},
+      ]);
 
        const onSortChange = (event) => {
             const value = event.value.value;
             const sortValue = event.value;
-         
-       
-
             if (value.indexOf('!') === 0) {
                 sortOrder.value = -1;
                 sortField.value = value.substring(1, value.length);
@@ -138,6 +128,7 @@ export default {
     },
 }
 </script>
+
 <style lang="scss" scoped>
 ::v-deep(.p-grid){
   display: flex;
