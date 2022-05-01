b7c6850896267340bc42ce625e531bbb16eaaf03
benyamin parsa <benyaminparsa727@gmail.com>
2022-05-01 17:44:34 +0430

    fix

--- src/app/unit/pages/Teacher/mainScreen.vue
+++ src/app/unit/pages/Teacher/mainScreen.vue
@@ -4,6 +4,26 @@
        <span>درج اطلاعات درباره درس</span>
        <img src="@/assets/img/svg/arrow-icon.svg">
    </div>
+   <div class="child">
+       <span>درس های من</span>
+       <img src="@/assets/img/svg/arrow-icon.svg">
+   </div>
+   <div class="child">
+       <span>ساخت درس</span>
+       <img src="@/assets/img/svg/arrow-icon.svg">
+   </div>
+   <div class="child">
+       <span>صدور گواهینامه</span>
+       <img src="@/assets/img/svg/arrow-icon.svg">
+   </div>
+   <div class="child">
+       <span>ارسال پیام و اطلاعیه</span>
+       <img src="@/assets/img/svg/arrow-icon.svg">
+   </div>
+   <div class="child">
+       <span>ارسال گزارشات</span>
+       <img src="@/assets/img/svg/arrow-icon.svg">
+   </div>
  </div>    
 </template>
 <script>
@@ -25,6 +45,7 @@ export default {
         background-color:#ffff;
         padding: 15px;
         border-radius: 18px;
+        margin-top: 30px;
         span {
             color: rgba(107, 114, 128, 1);
         }
